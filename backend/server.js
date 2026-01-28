65fe77de6b1ba84ef25ffcc30061c5265a15ad4f
Ayush Sharma <your-email@example.com>
2026-01-28 13:37:55 +0530

    Frontend and backend url updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    'https://digital-cfse.vercel.app',
+    'https://digitalexpressindia.com/',
     'http://localhost:5173'
   ],
   credentials: true
